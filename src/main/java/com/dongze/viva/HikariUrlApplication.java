875a322af555d78e7ffb6c0cc0b3655a51f9234d
Nicholas <dongzeviva@msn.cn>
2020-10-20 23:48:06 +0800

    初始化项目

---
+++ src/main/java/com/dongze/viva/HikariUrlApplication.java
@@ -0,0 +1,14 @@
+package com.dongze.viva;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikariUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HikariUrlApplication.class);
+    }
+
+}
