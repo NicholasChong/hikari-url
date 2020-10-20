c2f553a7d6bb4bf7609150d0053558c4ade1dfd2
Nicholas <dongzeviva@msn.cn>
2020-10-21 00:07:20 +0800

    初始化项目

---
+++ src/test/java/com/dongze/viva/HikariUrlApplicationTest.java
@@ -0,0 +1,16 @@
+package com.dongze.viva;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HikariUrlApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HikariUrlApplicationTest {
+
+
+    @Test
+    public void test(){
+    }
+}
