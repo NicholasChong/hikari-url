530eb2c7d107af7e5c42d367cfc63729310dc494
Nicholas <dongzeviva@msn.cn>
2020-10-21 16:20:16 +0800

    更新 author

--- src/main/java/com/dongze/viva/HikariUrlApplication.java
+++ src/main/java/com/dongze/viva/HikariUrlApplication.java
@@ -4,6 +4,9 @@ package com.dongze.viva;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Nicholas
+ */
 @SpringBootApplication
 public class HikariUrlApplication {
 
